16ff81e8b26135b24b0348e2b664eb7e64c28d00
Ismail Ahmet Uludag <202360562@collegeahuntsic.qc.ca>
2025-11-04 19:10:24 -0500

    fix index

--- views/index.php
+++ views/index.php
@@ -20,26 +20,26 @@ nouvelle gamme d'été en soie unisexe. Son style, inspiré du mouvement vaporwa
 accentue les tons pastel tels que le bleu ciel, le rose poudré et le violet lavande, et a ainsi conquis le monde 
 de l'ultra-luxe.</p>
         </div>
-<main>
-    <h2>Produits phares</h2>
-    <div class="products-grid">
-        <article class="product">
-            <img src="../assets/images/tie1.webp" alt="Tie 1" class="product-image">
-        </article>
+        <div>
+            <h2>Produits phares</h2>
+            <div class="products-grid">
+                <article class="product">
+                    <img src="../assets/images/tie1.webp" alt="Tie 1" class="product-image">
+                </article>
 
-        <article class="product">
-            <img src="../assets/images/tie2.webp" alt="Tie 2" class="product-image">
-        </article>
+                <article class="product">
+                    <img src="../assets/images/tie2.webp" alt="Tie 2" class="product-image">
+                </article>
 
-        <article class="product">
-            <img src="../assets/images/tshirt1.webp" alt="T-shirt 1" class="product-image">
-        </article>
+                <article class="product">
+                    <img src="../assets/images/tshirt1.webp" alt="T-shirt 1" class="product-image">
+                </article>
 
-        <article class="product">
-            <img src="../assets/images/tshirt2.webp" alt="T-shirt 2" class="product-image">     
-        </article>
-    </div>
-</main>
+                <article class="product">
+                    <img src="../assets/images/tshirt2.webp" alt="T-shirt 2" class="product-image">     
+                </article>
+            </div>
+        </div>
     </body>
     <?php include __DIR__ . '/includes/footer.php'; ?>
 </html>
